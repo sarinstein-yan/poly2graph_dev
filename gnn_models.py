56e1e313a95e177a3cb49bef3d7bf8c3ad834f94
sarinstein-yan <98195226+sarinstein-yan@users.noreply.github.com>
2024-07-29 12:48:41 +0800

    Update gnn_models.py

--- gnn_models.py
+++ gnn_models.py
@@ -13,6 +13,7 @@ from torch_geometric.nn.conv import (
     GATv2Conv,
     GCNConv,
     GINConv,
+    GINEConv,
     MessagePassing,
     PNAConv,
     SAGEConv,
@@ -39,6 +40,7 @@ __all__ = [
     'GCN',
     'GraphSAGE',
     'GIN',
+    'GINE',
     'GAT',
     'PNA',
     'EdgeCNN',
@@ -779,6 +781,52 @@ class GIN(BasicGNN):
             norm_kwargs=self.norm_kwargs,
         )
         return GINConv(mlp, **kwargs)
+
+
+class GINE(BasicGNN):
+    r"""Strategies for Pre-training Graph Neural Networks" 
+    <https://arxiv.org/abs/1905.12265>`_ paper, using the modified
+    :class:`~torch_geometric.nn.GINEConv` operator for message passing.
+
+    Args:
+        in_channels (int): Size of each input sample.
+        hidden_channels (int): Size of each hidden sample.
+        num_layers (int): Number of message passing layers.
+        out_channels (int, optional): If not set to :obj:`None`, will apply a
+            final linear transformation to convert hidden node embeddings to
+            output size :obj:`out_channels`. (default: :obj:`None`)
+        dropout (float, optional): Dropout probability. (default: :obj:`0.`)
+        edge_dim (int, optional): Edge feature dimensionality. If set to
+            :obj:`None`, node and edge feature dimensionality is expected to
+            match. Other-wise, edge features are linearly transformed to match
+            node feature dimensionality. (default: :obj:`None`)
+        act (str or Callable, optional): The non-linear activation function to
+            use. (default: :obj:`"relu"`)
+        act_first (bool, optional): If set to :obj:`True`, activation is
+            applied before normalization. (default: :obj:`False`)
+        act_kwargs (Dict[str, Any], optional): Arguments passed to the
+            respective activation function defined by :obj:`act`.
+            (default: :obj:`None`)
+        norm (str or Callable, optional): The normalization function to
+            use. (default: :obj:`None`)
+        norm_kwargs (Dict[str, Any], optional): Arguments passed to the
+            respective normalization function defined by :obj:`norm`.
+            (default: :obj:`None`)
+        jk (str, optional): The Jumping Knowledge mode. If specified, the model
+            will additionally apply a final linear transformation to transform
+            node embeddings to the expected output feature dimensionality.
+            (:obj:`None`, :obj:`"last"`, :obj:`"cat"`, :obj:`"max"`,
+            :obj:`"lstm"`). (default: :obj:`None`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.GINEConv`.
+    """
+    supports_edge_weight: Final[bool] = False
+    supports_edge_attr: Final[bool] = True
+    supports_norm_batch: Final[bool]
+
+    def init_conv(self, in_channels: int, out_channels: int,
+                  **kwargs) -> MessagePassing:
+        return GINEConv(in_channels, out_channels, **kwargs)
 
 
 class GAT(BasicGNN):
